35d016d7ae436b50952d870bb349c0951478791b
Michael Sokol <mikaa123@gmail.com>
2010-11-03 08:37:51 -0400

    adding sparql

--- hello.rb
+++ hello.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
 require 'sinatra'
+require 'SparqlTransmission'
 
 get '/' do
   "Hello from Sinatra on Heroku!"
+  sq = SparqlTransmission.new 'http://dbpedia.org/sparql'
+  sq.query = "SELECT ?subject 
+  WHERE 
+  {
+
+   ?subject <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/Person>
+
+  }"
+  
+  puts sq.execute_query
 end
