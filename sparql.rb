4b330e127d48479614ce1fcee7e4b52212f413b9
Michael Sokol <mikaa123@gmail.com>
2010-11-05 11:50:13 -0400

    First version

--- sparql.rb
+++ sparql.rb
@@ -15,11 +15,11 @@ get '/results' do
   st.execute_query
   
   var = {
-    results:    st.results,
-    variables:  st.requested_variables,
-    total_res:  st.results.length,
-    full_url:   "/results?query=#{URI.encode(params[:query])}&full=true",
-    xml_url:    "/results.xml?query=#{URI.encode(params[:query])}"
+    :results   => st.results,
+    :variables => st.requested_variables,
+    :total_res => st.results.length,
+    :full_url  => "/results?query=#{URI.encode(params[:query])}&full=true",
+    :xml_url   => "/results.xml?query=#{URI.encode(params[:query])}"
   }
   haml :index, :locals => {:var => var}
 end
